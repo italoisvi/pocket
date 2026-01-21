9e14ee3142f5f0779fcc010eccc6a16f81ab7489
Italo Isvi <italoisvi@hotmail.com>
2026-01-21 00:37:55 -0300

    Implementando a voz do walts - parte 1

--- app.config.js
+++ app.config.js
@@ -3,7 +3,7 @@ export default {
     owner: 'gladius-sistemas',
     name: 'Pocket',
     slug: 'pocket',
-    version: '1.4.1',
+    version: '1.5.1',
     orientation: 'portrait',
     icon: './assets/images/kanguroologin.png',
     userInterfaceStyle: 'automatic',
